4f01db81c9ab802fd6ef5bd22956c36c1c433740
陳湞欽 <jhencin.chen@iisigroup.com>
2026-07-23 11:19:10 +0800

    style: move chat bubble to side of kiwi to prevent overlap

--- src/main/main.js
+++ src/main/main.js
@@ -25,8 +25,8 @@ let historyWin = null;
 function createWindow() {
   const primaryDisplay = screen.getPrimaryDisplay();
   const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
-  const winWidth = 250;
-  const winHeight = 250;
+  const winWidth = 450;
+  const winHeight = 260;
   
   const randomX = Math.floor(Math.random() * (screenWidth - winWidth));
   const randomY = Math.floor(Math.random() * (screenHeight - winHeight));
